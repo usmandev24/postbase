9c90860fed44c1d96004e8ae95a142cb4be52f9e
Usman Ghani <usman.dev24@gmail.com>
2026-02-24 12:02:43 +0500

    feat( ui ) : Landing page intro add, styles updated
    explore catge header styles update

--- routes/index.mjs
+++ routes/index.mjs
@@ -2,7 +2,6 @@ import * as express from 'express';
 import { WsServer } from '../app.mjs';
 import { catgsStore } from './posts.mjs';
 import PrismaPostsStore from "../models/posts-prisma.mjs"
-import { totalmem } from 'os';
 
 export const router = express.Router();
 export const postsStore = new PrismaPostsStore()
@@ -27,13 +26,16 @@ export function wsHomeListners() {
 }
 router.get("/explore/:catgName", async (req, res, next) => {
   const keylist = await catgsStore.getPostKeysByCatg(req.params.catgName)
+  const catgNameList = (await catgsStore.getCategoriesNames()).map(v => {
+    return {catgName: v}
+  })
   const postlist = await Promise.all(keylist.map(key => postsStore.read(key)))
   res.render("index", {
-    title: req.params.catgName,
+    title: "Exploring " + req.params.catgName,
     postlist: postlist,
     catgName: req.params.catgName,
     total: postlist.length,
-    
+    catgNameList,
     user: req.user ? req.user : undefined,
     level: req.query.level
   })
@@ -43,7 +45,7 @@ router.get("/explore/:catgName", async (req, res, next) => {
 router.get('/', async (req, res, next) => {
   try {
 
-    const keylist = await postsStore.keylist();
+    const keylist = (await postsStore.keylist()).filter((v, i) => i <= 2);
     const keyPromises = keylist.map(key => {
       return postsStore.read(key);
     });
